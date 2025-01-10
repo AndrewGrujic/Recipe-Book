5d0a6ae328bb0c1ce28c42803103784f7a717193
OneUpAndy <78950797+OneUpAndy@users.noreply.github.com>
2025-01-10 16:42:58 +0000

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RecipeBook {
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window {
+		List<Recipe> recipeList = new List<Recipe>();
+		public MainWindow() {
+			InitializeComponent();
+		}
+
+		private void AddRecipeButton_Click(object sender, RoutedEventArgs e) {
+			recipeList.Add(new Recipe { recipeName = "New Recipe" });
+			RecipeListBox.Items.Add(recipeList[recipeList.Count - 1].recipeName);
+			RecipeListBox.SelectedIndex = RecipeListBox.Items.Count - 1;
+		}
+	}
+}
---
+++ Recipe.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecipeBook {
+	class Recipe {
+		public required string recipeName;
+		public List<String>? recipeIngredient;
+	}
+
+    
+}
